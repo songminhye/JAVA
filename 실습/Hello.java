0dc0f27c1963beed78fc2acdbc765a5fd1467c4d
songminhye <alsgp_46@naver.com>
2018-11-18 20:22:43 +0900

    d

--- 실습/Hello.java
+++
@@ -1,5 +0,0 @@
-public class Hello{
-public static void main(String args[]) {
-System.out.print("¾È³ç¾È³ç Hello world");
-}
-}
--- 실습/Lab1.java
+++
@@ -1,7 +0,0 @@
-public class Lab1 {
- public static void main(String args[]){
-
-System.out.println("Hello world!");
-System.out.println("I'm a new Java programmer");
-}
-}
